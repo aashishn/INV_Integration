55ef0f15d425cd2879a968b8eaa6dcb48ecaa0a0
aashishn <ashish.naravanekar@gmail.com>
2016-10-05 16:47:27 +0530

    BackLog Processor Update

--- src/main/java/com/serene/web/job/processor/BacklogItemProcessor.java
+++ src/main/java/com/serene/web/job/processor/BacklogItemProcessor.java
@@ -19,8 +19,11 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Service;
 
 import com.serene.job.common.BatchJobContext;
+import com.serene.job.repo.dao.JobSchedulerMetadataDao;
+import com.serene.job.repo.model.JobSchedulerMetadata;
 import com.serene.job.service.UpdateResponseService;
 import com.serene.job.util.CacheUtil;
+import com.serene.job.util.JobUtils;
 import com.serene.job.util.PayloadBuilder;
 import com.serene.ws.service.FusionWebService;
 
@@ -31,7 +34,10 @@ public class BacklogItemProcessor implements ItemProcessor<Object, Object> {
 	private static final Log log = LogFactory.getLog(BacklogItemProcessor.class);
 	@Resource
 	protected UpdateResponseService updateResponseService;
-
+	
+	@Resource 
+	protected JobUtils jobUtils ;
+	
 	@Resource(name = "fusionWebServiceImpl")
 	protected FusionWebService fusionWebService;
 	@Resource
@@ -41,6 +47,8 @@ public class BacklogItemProcessor implements ItemProcessor<Object, Object> {
 	protected PayloadBuilder payloadBuilder;
 	@Resource
 	private CacheUtil cacheUtil;
+	@Resource
+	private JobSchedulerMetadataDao jobSchedulerMetadataDao;
 	
 	@SuppressWarnings("unchecked")
 	@Override
@@ -66,8 +74,7 @@ public class BacklogItemProcessor implements ItemProcessor<Object, Object> {
 		         SqlParameterSource namedParameters = new MapSqlParameterSource("EBS_TRANSACTION_ID", recordName);
 		         backlogCount=jdbcTemplate.queryForObject(sql, namedParameters, Integer.class);
 			 }
-			
-	         System.out.println("BackLog Count is "+backlogCount + " for id   +"+id);
+
 			// If this does not exist in EBS, delete it
 			if(backlogCount==0)		
 			{
@@ -77,13 +84,34 @@ public class BacklogItemProcessor implements ItemProcessor<Object, Object> {
 						this.batchJobContext.getCurrentObject().getItemReader().getObjectName());
 
 				this.updateResponseService.updateResponse(data, operationTypes, response);
+		        this.jobUtils.logItem(data, "", "COMPLETED", "DELETED SUCCESSFULLY FROM SALES CLOUD");
 			}
+			else
+	        this.jobUtils.logItem(data, "", "COMPLETED", null);
 			
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error("Error while deleting data ",e);
+            this.logErrorInDb(data,e);
 		}
-
+		updateMetadata(data);
 		return null;
 	}
-
+	
+	  public void updateMetadata(Map<String, Object> data)
+	  {
+		  JobSchedulerMetadata jobSchedulerMetadata = (JobSchedulerMetadata)this.jobSchedulerMetadataDao.findOne(this.batchJobContext.getCurrentObject().getJobName());
+		    Object lastUpdatedTime = data.get(jobSchedulerMetadata.getInterfaceLastUpdatedField());
+		    if (lastUpdatedTime != null) {
+		         jobSchedulerMetadata.setInterfaceLastPoolTime(lastUpdatedTime.toString());
+		     }
+		     this.jobSchedulerMetadataDao.save(jobSchedulerMetadata);
+	  }
+	  
+	   public void logErrorInDb(Map<String, Object> item, Exception e) {
+	        try {
+	            this.jobUtils.logItemErrorStatus(e, item);        }
+	        catch (Exception e1) {
+	            log.error("Error while writing error log ", (Throwable)e1);
+	        }
+	    }
 }
